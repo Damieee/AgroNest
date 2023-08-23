080fcd1ccc661ab505fe4959a7251d4d71e7ee4d
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-08-23 11:40:13 +0100

    Add: remove redundant code

--- AgroNest_Backend/server.py
+++ AgroNest_Backend/server.py
@@ -2,18 +2,17 @@ import uvicorn
 from core.settings import settings
 from core.app import get_app
 
-
 def main() -> None:
     """Entrypoint of the application."""
+    app = get_app()  # Get the FastAPI app instance
     uvicorn.run(
-        "main:get_app",
+        app,
         workers=settings.WORKERS_COUNT,
         host=settings.HOST,
         port=settings.PORT,
         reload=settings.RELOAD,
         factory=True,
     )
-
 
 if __name__ == "__main__":
     main()
